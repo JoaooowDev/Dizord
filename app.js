dec8177e2ba3bcfcdfc93d3f6c0a47e1e230bdab
JoaooowDev <joaovictor.oliveira290502@gmail.com>
2022-08-13 00:13:41 -0300

    Iniciando Back-end

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+require('dotenv').config();
 const express = require ('express');
 const app = express();
-const port = 3000
+const PORT = process.env.PORT;
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/pages'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(PORT, () => {
+    console.log('Servidor iniciado na porta ' + PORT)
+})
