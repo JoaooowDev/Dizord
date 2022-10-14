1a75c099f764f957387aebec26419cdb7f9a44c8
JoaooowDev <joaovictor.oliveira290502@gmail.com>
2022-10-14 04:02:46 -0300

    Backand

--- models/DiscordServer.js
+++ models/DiscordServer.js
@@ -4,7 +4,7 @@ const serverSchema = new mongoose.Schema({
     idServidor: { type: Number, required: true},
     nomeServidor: { type: String, required: true },
     Plano: { type: String, required: true, default: 'free'},
-    Booster: { type: Array, required: true}
+    Booster: { type: Object, required: true}
 })
 
 const DiscordServer = module.exports = mongoose.model('servidores', serverSchema)
